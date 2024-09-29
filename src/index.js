e861d483ce83050be10856a577b4afa836cbf45e
PolinaRomanchuk <polinar_99@mail.ru>
2024-09-29 21:29:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+let stringNumber = Math.abs(n).toString();
+let arrNumber = stringNumber.split("");
+let arrRevers = arrNumber.reverse();
+return Number(arrRevers.join(""));
 }
